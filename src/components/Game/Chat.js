68517fb267fa95b138783ce785cad40221acdf4d
unknown <aaronjbraithwaite@gmail.com>
2018-12-19 09:04:35 -0700

    wednesday morning

--- src/components/Game/Chat.js
+++ src/components/Game/Chat.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Chat.css';
 
 class Chat extends Component{
     constructor(props){
         super(props);
 
         this.state = {
-
+            messagesArray:[]
         }
     }
 
@@ -13,10 +14,28 @@ class Chat extends Component{
 
     }
 
+    displayAllMessages(){
+        return this.state.messagesArray.map((element,index,arr)=>{
+            return (
+            <div className='text-carrier'>
+            <p className='message'>Sender: {element.sender}</p>
+                <p className='message'>Message: {element.message}</p>
+                </div>)
+        
+        })
+    }
+
     render(){
         return (
-            <div>
-                
+            <div className = 'chat-thing'>
+                <div className = 'chatBox'>
+                    {this.displayAllMessages()}
+                </div>
+                <div className='creator-of-space'></div>
+                <div className='sender-of-message'>
+                    <input className='input-box'></input>
+                    <button className='input-button'>Send Message</button>
+                </div>
             </div>
         )
     }
