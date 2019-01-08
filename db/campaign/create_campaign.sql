b776b92e3e6f50c833961cc9bee3399dafc853c8
unknown <mattgvo@gmail.com>
2019-01-08 12:11:09 -0700

    campaigns have default boards on creation and delete all boards on deletion

--- db/campaign/create_campaign.sql
+++ db/campaign/create_campaign.sql
@@ -1,3 +1,5 @@
 INSERT INTO campaign_info 
 (user_id, campaign_name, room_code) 
 VALUES ((select user_id from user_info where user_email = $1), $2, $3);
+
+INSERT INTO board_info (campaign_id, board_name, board_col, board_row) VALUES ((SELECT campaign_id FROM campaign_info WHERE campaign_name = $2 ) , 'default', 5, 5);
