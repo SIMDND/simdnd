53edca440bd7e5669773070e0223d0b46c0f6d4b
unknown <aaronjbraithwaite@gmail.com>
2019-01-08 12:18:06 -0700

    after styling Board selection stuff

--- server/campctrl.js
+++ server/campctrl.js
@@ -14,30 +14,34 @@ module.exports = {
         res.status(200).send(allCamps)
     },
     async editCampaignName(req,res){
-        console.log(req.body)
         const {campName, newCampName} = req.body
         const {user_email} = req.session.user
         let db = req.app.get('db');
-        console.log(db.campaign)
-        let newCampName = await db.campaign.update_campaign([newCampName,user_email, campName])
+        let a = await db.campaign.update_campaign([newCampName,user_email, campName])
         let allCamps = await db.campaign.get_campaigns([user_email])
         res.status(200).send(allCamps)
     },
     async editRoomCode(req,res){
-        console.log(req.body)
         const {campName, newRoomCode} = req.body
         const {user_email} = req.session.user
         let db = req.app.get('db');
-        console.log(db.campaign)
-        let newRoomCode = await db.campaign.update_room_code([newRoomCode, user_email, campName])
+        let a = await db.campaign.update_room_code([newRoomCode, user_email, campName])
         let allCamps = await db.campaign.get_campaigns([user_email])
+        res.status(200).send(allCamps)
+        
+    },
+    async editNameRoom(req,res){
+        const {campName, newRoomCode, newCampName} = req.body;
+        const {user_email} = req.session.user;
+        let db = req.app.get('db');
+        let a = await db.campaign.update_room_code([newRoomCode, user_email, campName]);
+        let b = await db.campaign.update_campaign([newCampName, user_email, campName]);
+        let allCamps = await db.campaign.get_campaigns([user_email]);
         res.status(200).send(allCamps)
     },
     async deleteCampaign(req,res){
         const {campName} = req.params
         const {user_email} = req.session.user
-        console.log('campName:',campName)
-        console.log('user_email:',user_email)
         let db = req.app.get('db');
         let deleteCamp = await db.campaign.delete_campaign([user_email, campName])
         let allCamps = await db.campaign.get_campaigns([user_email])
