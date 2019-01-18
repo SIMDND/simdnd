5edde31d68f5bf85a45eca2e6fdd10cfcb6c8da2
unknown <aaronjbraithwaite@gmail.com>
2019-01-18 16:03:46 -0700

    after finishing endpoint tests

--- server/endpoint_tests/get_pieces_aaron.js
+++ server/endpoint_tests/get_pieces_aaron.js
@@ -2,9 +2,108 @@
 
 //localhost:3674/piece/get-pieces/52/Charmander
 
+/*
+
 const pieces = pm.response.json()
 
 pm.test('has the correct character name', () => {
     pm.expect(pieces[1].character_name).to.equal("Vaporeon")
 })
 
+*/
+
+//Get Request
+
+//localhost:3674/piece/get-pieces/52/Charmander
+
+/*
+
+const pieces = pm.response.json();
+
+pm.test("Status code is 200", ()=>{
+    pm.response.to.have.status(200);
+});
+
+*/
+
+//Post Request
+
+// localhost:3674/piece/create
+
+/*
+body - {
+	"campaign_id":52,
+	"board_name":"Charmander",
+	"character_name":"Ben Affleck",
+	"piece_type":"npc",
+	"x_coordinate":3,
+	"y_coordinate":5,
+	"image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Ben_Affleck_by_Gage_Skidmore_3.jpg/220px-Ben_Affleck_by_Gage_Skidmore_3.jpg"
+}
+
+*/
+
+/*
+    const pieces = pm.response.json();
+
+    let a = pieces.findIndex(element=>element.character_name==='Ben Affleck');
+
+    pm.test('contains added item', function(){
+        pm.expect(a).to.not.equal(-1);
+        
+})
+
+*/
+
+//Delete Request
+
+//localhost:3674/piece/delete/52/Charmander/Ben Affleck
+
+/*
+
+const pieces = pm.response.json()
+
+let a = pieces.findIndex(element=>element.character_name==='Ben Affleck');
+
+pm.test('Does not contain deleted Item', function(){
+        pm.expect(a).to.equal(-1);
+        
+})
+
+*/
+
+//Put request
+
+//localhost:3674/piece/edit
+
+/*
+body - 
+    {
+	"campaign_id":52,
+	"board_name":"Charmander",
+	"new_character_name":"mewFour"
+	"character_name":"Ben Affleck",
+	"piece_type":"npc",
+	"x_coordinate":3,
+	"y_coordinate":5,
+	"image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Ben_Affleck_by_Gage_Skidmore_3.jpg/220px-Ben_Affleck_by_Gage_Skidmore_3.jpg"
+}
+*/
+
+/*
+    const pieces = pm.response.json();
+
+let numberOfTimesBenAffleckIsSeen = 0;
+
+pieces.forEach((element,index,arr)=>{
+    if (element.character_name==='Ben Affleck'){
+        numberOfTimesBenAffleckIsSeen++;
+    }
+})
+
+pm.test('Does not contain edited name', function(){
+        pm.expect(numberOfTimesBenAffleckIsSeen).to.equal(0);
+        
+})
+
+*/
--- src/tests/logic.test.js
+++ src/tests/logic.test.js
@@ -1,5 +1,11 @@
 //destructure the logic functions
 const {toggle} = require('../logic/logic_matt')
+const {cloneArray} = require('../logic/logic_aaron')
+const getType = require('jest-get-type');
+const {mergeArrays} = require('../logic/logic_aaron')
+const {getPlayerTokens} = require('../logic/logic_aaron')
+const {shuffleTokens} = require('../logic/logic_aaron')
+const {checkForPlayers} = require('../logic/logic_aaron')
 
 describe('Tests Toggle: The Toggle Tester', () => {
     test('if given false returns true', () => {
@@ -14,5 +20,40 @@ describe('Tests the toggle functionality', () =>{
     test('if given true, returns false', ()=> {
         var answer = toggle(true)
         expect(answer).toBe(false)
+    })
+})
+
+describe('Clone Array Tester', () =>{
+    test("return type should be an array", () => {
+        let a = cloneArray([{id:0,name:'a'},{id:1,name:'b'}])
+        expect(getType(a)).toBe('array')
+	})
+})
+
+describe('Merge Arrays tester', () =>{
+    test("Merge array return value should have no property x_coordinate", () =>{
+        let a = mergeArrays([{id:0,x:3,y:2,type:'Baggai'}],[{character_name:"aaron",piece_type:"player",x_coordinate:0,y_coordinate:0,image_url:'taco'}]);
+        expect(a[1].x_coordinate).toBe(undefined);
+    })
+})
+
+describe('Get Player Tokens Tester', () =>{
+    test('Return value of getPlayerTokens should not contain any types npc',()=>{
+        let a = getPlayerTokens([{piece_type:'npc',character_name:'Fred'},{piece_type:'player',character_name:'George'}]);
+        expect(a[0].piece_type !== 'npc').toBe(true)
+    })
+})
+
+describe('Shuffle Tokens Test', () =>{
+    test('Return value of shuffle tokens should be same length as tokens passed in', ()=>{
+        let a = shuffleTokens([{id:0,name:'Charlie'},{id:1,name:'Delta'}]);
+        expect(a.length).toBe(2);
+    })
+})
+
+describe('Check for Players Test', () =>{
+    test('Array with element that has piece_type of player should return true', ()=>{
+        let a = checkForPlayers([{piece_type:'npc',name:'Charlie'},{piece_type:'player',name:'Delta'}]);
+        expect(a).toEqual(true);
     })
 })
